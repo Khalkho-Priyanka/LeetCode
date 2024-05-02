b4ee312e6414c0f030486e351c9316b5b16c85ba
Priyanka M. Khalkho <135851118+Khalkho-Priyanka@users.noreply.github.com>
2024-05-01 22:36:03 -0700

    Time: 1092 ms (90.31%), Space: 0B (100.00%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,4 @@
+SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
+FROM Employee e JOIN Department d
+ON d.id=e.departmentId 
+WHERE (e.salary, e.departmentId) IN (SELECT MAX(salary), departmentId FROM Employee GROUP BY departmentId);
